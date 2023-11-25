0c0956b4abb7988ed6675d772ad2e9fd826ff5ea
Kicks dev <luiskiqueromero@gmail.com>
2023-11-24 18:22:31 -0600

    change lista primos a funcion para obtener primos

--- src/Formula/index.js
+++ src/Formula/index.js
@@ -1,29 +1,47 @@
+const calcTriangle = (n) => {
+  return (n * (n + 1)) / 2;
+};
 
-const calcTriangle = (n) =>{
-    return ((n*(n+1))/2)
-}
+const fibonacci = (n) => {
+  let a = 0,
+    b = 1,
+    c = n;
 
-const fibonacci = n => {
-    let a = 0, b = 1, c = n;
-    
-    for(let i = 2; i <= n; i++) {
-      c = a + b;
-      a = b;
-      b = c;
+  for (let i = 2; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+
+  return c;
+};
+
+const esPrimo = (num) => {
+  let c = 300,
+    j = 2,
+    numerosPrimos = [];
+  for (; j < c; j++) {
+    if (primo(j)) {
+      numerosPrimos.push(j);
     }
-    
-    return c;
-  };
+  }
+  return numerosPrimos[num - 1];
+};
 
-  let listPrime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997 ]
-  const esPrimo = (num) =>{
-    return listPrime[num - 1]
+const primo = (numero) => {
+  for (let i = 2; i < numero; i++) {
+    if (numero % i === 0) {
+      return false;
+    }
   }
 
-  const serie= (num)=>{
-    if(num<=2) return 0
-    let opc = (2*calcTriangle(num+2)*3*esPrimo(num-2))/(7*fibonacci(num))
-    return Math.round(opc)
-  }
+  return numero !== 1;
+};
 
-  export default serie
+const serie = (num) => {
+  if (num <= 2) return 0;
+  let opc =
+    (2 * calcTriangle(num + 2) * 3 * esPrimo(num - 2)) / (7 * fibonacci(num));
+  return Math.round(opc);
+};
+export default serie;
